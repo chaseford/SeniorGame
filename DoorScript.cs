402be94995446d992aca0b8c8868b781168b9509
chaseford <chaseford@u.boisestate.edu>
2020-03-23 16:32:26 -0600

    Add files via upload

---
+++ DoorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private Animator _animator;
+
+    private bool _isInsideTrigger = false;
+
+    void Start()
+    {
+        _animator = transform.Find("Door").GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = false;
+            _animator.SetBool("open", false);
+        }
+    }
+
+    void Update()
+    {
+
+        if (_isInsideTrigger)
+        {
+            //Pushing "A" button on the Oculus controller when within the collider opens the door
+            if (OVRInput.GetDown(OVRInput.Button.One))
+            {
+                _animator.SetBool("open", true);
+            }
+        }
+    }
+}
---
+++ MusicClass.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+
+    public void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "YouWin")
+
+        {
+            StopMusic();
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+
+       if(sceneName == "You Lose")
+
+        {
+            StopMusic();
+
+            if(!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+
+    }
+
+}
